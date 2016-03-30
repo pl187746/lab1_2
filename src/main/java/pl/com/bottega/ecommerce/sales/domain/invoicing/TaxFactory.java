2850b999389247d1c7db420221590400b64b06f8
Karol Kucharski <187746@edu.p.lodz.pl>
2016-03-30 21:44:27 +0200

    Utworzenie interfejsu fabryki podatkow TaxFactory.

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxFactory.java
@@ -0,0 +1,10 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public interface TaxFactory {
+	
+	Tax createTax(Money net, ProductType productType);
+
+}
